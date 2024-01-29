7ae5170e7a4b33fc694ca93864bdddfb4579d252
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2024-01-29 18:49:30 +0100

    Banner part done

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,10 +1,33 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import banner1 from "../../../assets/home/1.png";
+// import banner2 from "../../../assets/home/2.jpeg";
+// import banner3 from "../../../assets/home/3.jpeg";
+// import banner4 from "../../../assets/home/4.jpeg";
+// import banner5 from "../../../assets/home/5.png";
+
+
+
+// const BannerImages = [banner1, banner2, banner3, banner4, banner5];
+const BannerImages = [banner1];
+
+
 
 
 const Banner = () => {
     return (
-        <div>
-            
-        </div>
+        <Carousel className="text-center"
+        showArrows={false}
+        autoPlay={true}
+        infiniteLoop={true}
+        showStatus={false}
+    >
+        {BannerImages.map((banner, index) => (
+            <div key={index}>
+                <img src={banner} />
+            </div>
+        ))}
+    </Carousel>
     );
 };
 
